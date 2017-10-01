fbc21a62ce5c61960ebe056d32a466f003508054
kevingilfether <kevingilfether@gmail.com>
2017-10-01 12:39:24 -0400

    practice conditionals inside while loop

--- Loop_practice/Program.cs
+++ Loop_practice/Program.cs
@@ -15,10 +15,69 @@ namespace Loop_practice
             //while loops perform actions as long as the loop is true
 
             //makes a loop that prints 1-10 on new lines
+            //int x = 1;
+            //while (x <= 10)
+            //{
+            //    Console.WriteLine(x);
+            //    x++;
+            //}
+
+            //makes a while loop where each digit from 1-10 is printed on each line
+            //with an indent corresponding to position
+
             int x = 1;
-            while (x <= 10)
+            string numTabs = "\t";
+            while(x <= 10)
             {
-                Console.WriteLine(x);
+                if (x == 1)
+                {
+                    Console.WriteLine("\t" + x);
+                }
+
+                else if (x == 2)
+                {
+                    Console.WriteLine("\t\t" + x);
+                }
+
+                else if (x == 3)
+                {
+                    Console.WriteLine("\t\t\t" + x);
+                }
+
+                else if (x == 4)
+                {
+                    Console.WriteLine("\t\t\t\t" + x);
+                }
+
+                else if (x == 5)
+                {
+                    Console.WriteLine("\t\t\t\t\t" + x);
+                }
+
+                else if (x == 6)
+                {
+                    Console.WriteLine("\t\t\t\t\t\t" + x);
+                }
+
+                else if (x == 7)
+                {
+                    Console.WriteLine("\t\t\t\t\t\t\t" + x);
+                }
+
+                else if (x == 8)
+                {
+                    Console.WriteLine("\t\t\t\t\t\t\t\t" + x);
+                }
+
+                else if (x == 9)
+                {
+                    Console.WriteLine("\t\t\t\t\t\t\t\t\t" + x);
+                }
+
+                else if (x == 10)
+                {
+                    Console.WriteLine("\t\t\t\t\t\t\t\t\t\t" + x);
+                }
                 x++;
             }
         }
